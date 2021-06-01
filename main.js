7d4c22c7b1077234f05945c3b405f2c958a4afa9
Franco Battagion <fran.battagion@gmail.com>
2021-06-01 18:26:58 -0300

    prototype of array and render

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
+var userBoard = new Array();
+userBoard[0] = new Array(1,2,3,3,3);
+userBoard[1] = new Array(2,3,5,4,1);
+userBoard[2] = new Array(5,4,3,4,2);
+userBoard[3] = new Array(4,2,1,5,3);
+userBoard[4] = new Array(3,5,4,2,1);
+// var boardLength = userBoard.length;
+
 var app = new Vue({
     el: '#app',
     data: {
-      message: '',
-      },
+      message: ''
+    } 
+      ,
       data() {
         return {
             counter: 0,
-            message: 'testMessage'
+            message: 'broken11',
+            userBoard: userBoard
         }
     }
         
   })
 
- 
+
+
+  // var userBoard = new Array(1,2,3,1,2,3,3,2,1)
 
