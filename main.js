bc4c582c0cdf8d978f0d72b8ce5c30bca9d0dadc
Franco Battagion <fran.battagion@gmail.com>
2021-06-01 09:02:03 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: '',
+      },
+      data() {
+        return {
+            counter: 0,
+            message: 'testMessage'
+        }
+    }
+        
+  })
+
+ 
+
