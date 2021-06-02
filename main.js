c3ba415eb55bf9ffc7747095542e6821e561d166
Franco Battagion <fran.battagion@gmail.com>
2021-06-02 15:53:42 -0300

    added array savedToReset[] that stores id of selected blocks to be later used by deselectionOut() -- para deseleccionar bloques ---

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const userBoard = document.querySelector('.b0-board')
   const blocks = []
+  var resetId
+  var savedToReset = []  // or to destroy -- sirve para destruir tambien
   
   const colors = [
       '#FF71EA', // pink
@@ -9,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       '#B4FFE2', // light emerald
       '#A0FF33', // lime
     ]
-  const colorsActive = '#fff'
+  const colorsActive = ''
   
   function createBoard() {
     for (let i = 0; i < 49; i++) {  // 5 x 5
@@ -27,18 +29,43 @@ document.addEventListener('DOMContentLoaded', () => {
   let blockIsSelected
   let colorOfSelectedBlock
   // let setBackgroundToSelected
-
+  let outerRim = document.querySelector('.b0-outer-rim')
 
   blocks.forEach(block => block.addEventListener('click', blockSelection))
-  
+ 
 
   function blockSelection() {
     blockIsSelected = parseInt(this.id)
     colorOfSelectedBlock = this.style.backgroundColor
     console.log(blockIsSelected,colorOfSelectedBlock)  // testing
-    this.style.backgroundColor = colorsActive
+    // this.style.backgroundColor = colorsActive
+    this.setAttribute('class', 'has-background-light button is-loading' )
+    // save id to reset in case user wants, guardo en array para deselect
+    // se puede usar tambien para ver los id de bloques a destruir
+    // falta agregar una tecla para completar la seleccion y que se destruyan los bloques
+
+    resetId = blockIsSelected
+    savedToReset.push(resetId)
+    for (let index = 1; index < 6; index++) {
+      savedToReset[index] = resetId
+     
+    }
   }
 
+
+  outerRim.addEventListener('click', deselectionOut)
+
+  for (let index = 0; index < savedToReset.length; index++) {
+    console.log('hola', savedToReset[index]) // testeando borrar despues
+   }
+   // deselect - reset function vuelven a su color original
+  function deselectionOut() {
+    for (let index = 0; savedToReset.length; index++) {
+      const element = savedToReset[index]
+    document.getElementById(element).removeAttribute('class','has-background-light button is-loading' )
+   }
+    
+  }
 
 })
   
