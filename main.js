84b8af483cad149d0b3c7622252eeec149ad0de3
Franco Battagion <fran.battagion@gmail.com>
2021-06-02 09:29:33 -0300

    switch to js for board creation, commented vue cdn in index

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-var userBoard = new Array();
-userBoard[0] = new Array(1,2,3,3,3);
-userBoard[1] = new Array(2,3,5,4,1);
-userBoard[2] = new Array(5,4,3,4,2);
-userBoard[3] = new Array(4,2,1,5,3);
-userBoard[4] = new Array(3,5,4,2,1);
-// var boardLength = userBoard.length;
-
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: ''
-    } 
-      ,
-      data() {
-        return {
-            counter: 0,
-            message: 'broken11',
-            userBoard: userBoard
-        }
+document.addEventListener('DOMContentLoaded', () => {
+  const userBoard = document.querySelector('.b0-board')
+  const blocks = []
+  
+  const colors = [
+      '#FF71EA', // pink
+      '#FFE964', // yellow
+      '#28E5FF', // bright light blue
+      '#B4FFE2', // light emerald
+      '#A0FF33', // lime
+    ]
+  
+  function createBoard() {
+    for (let i = 0; i < 25; i++) {  // 5 x 5
+      const block = document.createElement('button')
+      block.setAttribute('id', i)
+      block.setAttribute('class', 'button')
+      let colorLoad = Math.floor(Math.random() * colors.length)
+      block.style.backgroundColor = colors[colorLoad]
+      userBoard.appendChild(block)
+      blocks.push(block)
     }
-        
-  })
-
-
-
-  // var userBoard = new Array(1,2,3,1,2,3,3,2,1)
-
+  }
+  createBoard()
+})
+  
