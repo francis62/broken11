2eda7193f6db5968d4e525a8fa1363173a4da623
Franco Battagion <fran.battagion@gmail.com>
2021-06-03 08:35:47 -0300

    added completeSelection() and refreshBoard() functions, rearranged code

--- main.js
+++ main.js
@@ -3,16 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const blocks = []
   var resetId
   var savedToReset = []  // or to destroy -- sirve para destruir tambien
-  
+  var savedToDestroy = []
   const colors = [
-      '#FF71EA', // pink
-      '#FFE964', // yellow
-      '#28E5FF', // bright light blue
+      '#FF71EA', // pink rosa
+      '#FFE964', // yellow amarillo
+      '#28E5FF', // bright light blue celeste
       '#B4FFE2', // light emerald
-      '#A0FF33', // lime
+      '#A0FF33', // lime lima verda
     ]
-  const colorsActive = ''
+  const colorsActive = '' // no se esta usando
+  let blockIsSelected  // blockSelection()
+  let colorOfSelectedBlock // blockSelection()
+  let outerRim = document.querySelector('.b0-outer-rim') // blockSelection() deselectionOut()
+  outerRim.addEventListener('click', deselectionOut)
+  let destroyButton = document.querySelector('.b0-destroy-ok') // completeSelection()
+  destroyButton.addEventListener('click', completeSelection) // completeSelection()
+  let refreshButton = document.querySelector('.b0-refresh')  // refreshBoard()
+  refreshButton.addEventListener('click', refreshBoard) // refreshBoard()
   
+  function refreshBoard() {
+    location.reload()
+  }
+
+
   function createBoard() {
     for (let i = 0; i < 49; i++) {  // 5 x 5
       const block = document.createElement('button')
@@ -24,40 +37,32 @@ document.addEventListener('DOMContentLoaded', () => {
       blocks.push(block)
     }
   }
+
   createBoard()
-
-  let blockIsSelected
-  let colorOfSelectedBlock
-  // let setBackgroundToSelected
-  let outerRim = document.querySelector('.b0-outer-rim')
-
   blocks.forEach(block => block.addEventListener('click', blockSelection))
- 
+  
 
   function blockSelection() {
     blockIsSelected = parseInt(this.id)
     colorOfSelectedBlock = this.style.backgroundColor
     console.log(blockIsSelected,colorOfSelectedBlock)  // testing
-    // this.style.backgroundColor = colorsActive
+    this.removeAttribute('class','b0-destroy-queue is-disabled' )
     this.setAttribute('class', 'has-background-light button is-loading' )
+    
     // save id to reset in case user wants, guardo en array para deselect
     // se puede usar tambien para ver los id de bloques a destruir
     // falta agregar una tecla para completar la seleccion y que se destruyan los bloques
 
     resetId = blockIsSelected
     savedToReset.push(resetId)
+    savedToDestroy.push(resetId)
     for (let index = 1; index < 6; index++) {
       savedToReset[index] = resetId
-     
+      savedToDestroy[index] = resetId
     }
+
   }
 
-
-  outerRim.addEventListener('click', deselectionOut)
-
-  for (let index = 0; index < savedToReset.length; index++) {
-    console.log('hola', savedToReset[index]) // testeando borrar despues
-   }
    // deselect - reset function vuelven a su color original
   function deselectionOut() {
     for (let index = 0; savedToReset.length; index++) {
@@ -65,6 +70,15 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById(element).removeAttribute('class','has-background-light button is-loading' )
    }
     
+  }
+
+  function completeSelection() {
+    for (let index = 0; savedToDestroy.length; index++) {
+      const element = savedToDestroy[index]
+      const toBeDestroyed =  document.getElementById(element)
+    toBeDestroyed.removeAttribute('class','has-background-light button is-loading' )
+    toBeDestroyed.setAttribute('class','b0-destroy-queue button is-disabled' )
+   }
   }
 
 })
